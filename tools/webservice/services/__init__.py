1a1ea0cd458e544f2af4f3953da0bbc77e600b08
YuviPanda <yuvipanda@gmail.com>
2015-05-13 16:45:15 -0700

    Add lighttpd server types
    Adds three variants of lighttpd:
    
    - A simple one, LighttpdWebService, that runs on trusty and
      has a PHP setup by default
    - LighttpdPreciseWebService, same as above but runs on precise
    - LighttpdPlainWebService, which does *not* have the default
      PHP turned on. This is to support T98818
    
    Bug: T98818
    Bug: T98441
    Change-Id: I052e2c5b92755c66c0af5971b487c2cc1a9e83dd

--- tools/webservice/services/__init__.py
+++ tools/webservice/services/__init__.py
@@ -1,4 +1,9 @@
 from tools.webservice.services.jswebservice import JSWebService
 from tools.webservice.services.pythonwebservice import PythonWebService
+from tools.webservice.services.lighttpdwebservice import LighttpdPlainWebService, LighttpdWebService, \
+    LighttpdPreciseWebService
 
-webservice_classes = {cls.NAME: cls for cls in [JSWebService, PythonWebService]}
+
+webservice_classes = {cls.NAME: cls
+                      for cls in [JSWebService, PythonWebService,
+                                  LighttpdWebService, LighttpdPreciseWebService, LighttpdPlainWebService]}
---
+++ tools/webservice/services/lighttpdwebservice.py
@@ -0,0 +1,131 @@
+import os
+from tools.webservice import WebService
+
+
+BASIC_CONFIG_TEMPLATE = """
+server.modules = (
+  "mod_setenv",
+  "mod_access",
+  "mod_accesslog",
+  "mod_alias",
+  "mod_compress",
+  "mod_redirect",
+  "mod_rewrite",
+  "mod_fastcgi",
+  "mod_cgi",
+)
+
+server.port = {port}
+server.use-ipv6 = "disable"
+server.username = "{username}"
+server.groupname = "{groupname}"
+server.core-files = "disable"
+server.document-root = "{home}/public_html"
+server.pid-file = "/var/run/lighttpd/{toolname}.pid"
+server.errorlog = "{home}/error.log"
+server.breakagelog = "{home}/error.log"
+server.follow-symlink = "enable"
+server.max-connections = 300
+server.stat-cache-engine = "fam"
+server.event-handler = "linux-sysepoll"
+ssl.engine = "disable"
+
+alias.url = ( "/{toolname}" => "{home}/public_html/" )
+
+index-file.names = ( "index.php", "index.html", "index.htm" )
+dir-listing.encoding = "utf-8"
+server.dir-listing = "disable"
+url.access-deny = ( "~", ".inc" )
+static-file.exclude-extensions = ( ".php", ".pl", ".fcgi" )
+
+accesslog.use-syslog = "disable"
+accesslog.filename = "{home}/access.log"
+
+include_shell "/usr/share/lighttpd/create-mime.assign.pl"
+
+cgi.assign = (
+  ".pl" => "/usr/bin/perl",
+  ".py" => "/usr/bin/python",
+  ".pyc" => "/usr/bin/python",
+)
+"""
+
+ENABLE_PHP_CONFIG_TEMPLATE = """
+fastcgi.server += ( ".php" =>
+        ((
+                "bin-path" => "/usr/bin/php-cgi",
+                "socket" => "/var/run/lighttpd/php.socket.{toolname}",
+                "max-procs" => 2,
+                "bin-environment" => (
+                        "PHP_FCGI_CHILDREN" => "2",
+                        "PHP_FCGI_MAX_REQUESTS" => "500"
+                ),
+                "bin-copy-environment" => (
+                        "PATH", "SHELL", "USER"
+                ),
+                "broken-scriptfilename" => "enable",
+                "allow-x-send-file" => "enable"
+         ))
+)
+"""
+
+
+class LighttpdPlainWebService(WebService):
+    """
+    A 'plain' Lighttpd Webserver, without PHP setup by default
+    """
+    NAME = 'lighttpd-plain'
+    QUEUE = 'webgrid-lighttpd'
+
+    def check(self):
+        # Check for a .lighttpd.conf file or a public_html
+        public_html_path = self.tool.get_homedir_subpath('public_html')
+        lighttpd_conf_path = self.tool.get_homedir_subpath('.lighttpd.conf')
+        if not (os.path.exists(public_html_path) or os.path.exists(lighttpd_conf_path)):
+            raise WebService.InvalidWebServiceException(
+                'Could not find a public_html folder or a .lighttpd.conf file '
+                'in your tool home.'
+            )
+
+    def build_config(self, port, config_template=BASIC_CONFIG_TEMPLATE):
+        config = config_template.format(
+            toolname=self.tool.name,
+            username=self.tool.username,
+            groupname=self.tool.username,
+            home=self.tool.home,
+            port=port
+        )
+        try:
+            with open(self.tool.get_homedir_subpath('.lighttpd.conf')) as f:
+                config += f.read()
+        except:
+            pass  # No customized file, not a big deal
+        return config
+
+    def run(self, port):
+        config = self.build_config(port)
+        config_path = os.path.join('/var/run/lighttpd/', self.tool.name)
+        with open(config_path, 'w') as f:
+            f.write(config)
+
+        os.execv('/usr/sbin/lighttpd', ['/usr/sbin/lighttpd', '-f', config_path, '-D'])
+
+
+class LighttpdWebService(LighttpdPlainWebService):
+    """
+    A Lighttpd Webserver with a default PHP setup
+    """
+    NAME = 'lighttpd'
+    QUEUE = 'webgrid-lighttpd'
+
+    def build_config(self, port, config_template=BASIC_CONFIG_TEMPLATE + ENABLE_PHP_CONFIG_TEMPLATE):
+        return super(LighttpdWebService, self).build_config(port, config_template)
+
+
+class LighttpdPreciseWebService(LighttpdWebService):
+    """
+    A Lighttpd Webserver with a default PHP setup, running on Ubuntu precise
+    """
+    NAME = 'lighttpd-precise'
+    QUEUE = 'webgrid-lighttpd'
+    RELEASE = 'precise'
