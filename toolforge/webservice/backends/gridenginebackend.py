wikimedia/operations-software-tools-webservice

commit 45f3b00c97d074a0adc1cd1ece77bb486d72e326

Reuse toolforge.common.tool.PROJECT in KubernetesBackend
We ended up with two different places in the code doing the same action
of reading /etc/wmcs-project to determine which project the webservice
script is being called from. A bit of editing can solve that.

Also fix a bug waiting to happen where the 'type' argument to Backend
constructors was shadowing the built-in global function 'type'.

Change-Id: I69aab87539bf1683f64f09e9d69da281288e1852

scripts/webservice
@@ -217,11 +217,6 @@ if __name__ == "__main__":
 
     tool = Tool.from_currentuser()
 
-    # In order to more fully enable toolsbeta, we need to know we are there.
-    if os.path.isfile("/etc/wmcs-project"):
-        with open("/etc/wmcs-project") as f:
-            project = f.readline().rstrip()
-
     # We use this logic rather than setting defaults in argparse.
     # This allows us to default to spec in service.manifest but override
     # by specifying --backend option on the CLI
@@ -314,7 +309,7 @@ if __name__ == "__main__":
         job = GridEngineBackend(tool, args.type, args.extra_args)
     else:
         job = KubernetesBackend(
-            tool, project, args.type, args.memory, args.cpu, args.extra_args
+            tool, args.type, args.memory, args.cpu, args.extra_args
         )
 
     tool.manifest["backend"] = backend

toolforge/webservice/backends/gridenginebackend.py
@@ -38,12 +38,14 @@ class GridEngineBackend(Backend):
         "generic": {"cls": GenericWebService},
     }
 
-    def __init__(self, tool, type, extra_args=None):
-        super(GridEngineBackend, self).__init__(tool, type, extra_args)
-        cfg = GridEngineBackend.CONFIG[type]
+    def __init__(self, tool, wstype, extra_args=None):
+        super(GridEngineBackend, self).__init__(tool, wstype, extra_args)
+        cfg = GridEngineBackend.CONFIG[self.wstype]
         self.webservice = cfg["cls"](tool, extra_args)
         self.queue = cfg.get("queue", "webgrid-generic")
-        self.name = "{type}-{toolname}".format(type=type, toolname=tool.name)
+        self.name = "{type}-{toolname}".format(
+            type=self.wstype, toolname=tool.name
+        )
         try:
             memlimit = "/data/project/.system/config/{}.web-memlimit".format(
                 self.tool.name

toolforge/webservice/backends/kubernetesbackend.py
@@ -7,6 +7,7 @@ import time
 
 import pykube
 
+from toolforge.common.tool import PROJECT
 from toolforge.common.utils import parse_quantity
 from toolforge.webservice.backends import Backend
 from toolforge.webservice.services import GenericWebService
@@ -155,11 +156,9 @@ class KubernetesBackend(Backend):
         },
     }
 
-    def __init__(
-        self, tool, project, type, mem=None, cpu=None, extra_args=None
-    ):
-        super(KubernetesBackend, self).__init__(tool, type, extra_args)
-        self.project = project
+    def __init__(self, tool, wstype, mem=None, cpu=None, extra_args=None):
+        super(KubernetesBackend, self).__init__(tool, wstype, extra_args)
+        self.project = PROJECT
         self.webservice = KubernetesBackend.CONFIG[type]["cls"](
             tool, extra_args
         )

toolforge/webservice/backends/backend.py
@@ -13,9 +13,9 @@ class Backend(with_metaclass(ABCMeta, object)):
     STATE_RUNNING = 1
     STATE_PENDING = 2
 
-    def __init__(self, tool, type, extra_args=None):
+    def __init__(self, tool, wstype, extra_args=None):
         self.tool = tool
-        self.type = type
+        self.wstype = wstype
         self.extra_args = extra_args
 
     @property

commit 1096b6df4a9855027c83331a5a93f85e6fa7f79f

Fix partial rename of "type" parameter to "wstype"
Correct a bug introduced in 45f3b00 where we missed some of the renaming
of the badly chosen "type" parameter to "wstype".

Bug: T249390
Change-Id: Iffffec5ef62385b54a30a8b089145e6b92e676c5

scripts/webservice
@@ -27,24 +27,24 @@ from toolforge.common import Tool
 
 
 BACKEND_DEFAULT = "kubernetes"
-GRIDENGINE_DEFAULT_TYPE = "lighttpd"
-KUBERNETES_DEFAULT_TYPE = "php7.3"
+GRIDENGINE_DEFAULT_WSTYPE = "lighttpd"
+KUBERNETES_DEFAULT_WSTYPE = "php7.3"
 
 # Make all `print()` output go to stderr by default
 print = functools.partial(print, file=sys.stderr)
 
 
-def format_types_block(config, default, leader="\n  * "):
-    """Pretty print a list of runtime types for a backend."""
-    types = []
+def format_wstypes_block(config, default, leader="\n  * "):
+    """Pretty print a list of runtime wstypes for a backend."""
+    wstypes = []
     for name, spec in config.items():
         extra = ""
         if name == default:
             extra = " (default)"
         elif spec.get("deprecated", False):
             extra = " (DEPRECATED)"
-        types.append("{}{}".format(name, extra))
-    return leader.join(sorted(types))
+        wstypes.append("{}{}".format(name, extra))
+    return leader.join(sorted(wstypes))
 
 
 def kube_quant(string):
@@ -109,12 +109,12 @@ def update_manifest(job, action, args):
     if action == "start":
         if (
             "web" not in job.tool.manifest
-            or job.tool.manifest["web"] != job.type
+            or job.tool.manifest["web"] != job.wstype
         ):
             job.tool.manifest["distribution"] = platform.linux_distribution()[
                 0
             ]
-            job.tool.manifest["web"] = job.type
+            job.tool.manifest["web"] = job.wstype
             if args.canonical:
                 job.tool.manifest["canonical"] = args.canonical
             if args.replicas != 1:
@@ -160,14 +160,14 @@ if __name__ == "__main__":
       Grid Engine backend:
         * {gridengine}
     """.format(
-        kubernetes=format_types_block(
+        kubernetes=format_wstypes_block(
             KubernetesBackend.CONFIG,
-            KUBERNETES_DEFAULT_TYPE,
+            KUBERNETES_DEFAULT_WSTYPE,
             leader="\n        * ",
         ),
-        gridengine=format_types_block(
+        gridengine=format_wstypes_block(
             GridEngineBackend.CONFIG,
-            GRIDENGINE_DEFAULT_TYPE,
+            GRIDENGINE_DEFAULT_WSTYPE,
             leader="\n        * ",
         ),
     )
@@ -200,7 +200,10 @@ if __name__ == "__main__":
         help="Redirect legacy URL to the canonical $TOOL.toolforge.org URL",
     )
     argparser.add_argument(
-        "type", nargs="?", metavar="TYPE", help="Type of webservice to start",
+        "wstype",
+        nargs="?",
+        metavar="TYPE",
+        help="Type of webservice to start",
     )
 
     allowed_actions = ["start", "stop", "status", "restart", "shell"]
@@ -299,7 +302,7 @@ if __name__ == "__main__":
                 cpu=template_args.cpu,
                 memory=template_args.memory,
                 replicas=template_args.replicas,
-                type=template_args.type,
+                wstype=template_args.wstype,
             )
             # The argument gathering behavior of extra args does not work as
             # a default on the parser. It also will cause problems if the
@@ -389,16 +392,16 @@ if __name__ == "__main__":
             print("--replicas is only for --backend=kubernetes")
             print("Please run again without this argument")
             sys.exit(1)
-        if args.type is None:
-            args.type = tool.manifest.get("web", GRIDENGINE_DEFAULT_TYPE)
-        acceptable_types = GridEngineBackend.CONFIG
+        if args.wstype is None:
+            args.wstype = tool.manifest.get("web", GRIDENGINE_DEFAULT_WSTYPE)
+        acceptable_wstypes = GridEngineBackend.CONFIG
     elif backend == "kubernetes":
         if args.release is not None:
             print("--release is not implemented for --backend=kubernetes")
             print("Please just pick an appropriate type instead")
             sys.exit(1)
-        if args.type is None:
-            args.type = tool.manifest.get("web", KUBERNETES_DEFAULT_TYPE)
+        if args.wstype is None:
+            args.wstype = tool.manifest.get("web", KUBERNETES_DEFAULT_WSTYPE)
 
         if args.cpu is None:
             args.cpu = tool.manifest.get("cpu", None)
@@ -409,7 +412,7 @@ if __name__ == "__main__":
         if args.replicas == 1:
             args.replicas = tool.manifest.get("replicas", 1)
 
-        acceptable_types = KubernetesBackend.CONFIG
+        acceptable_wstypes = KubernetesBackend.CONFIG
 
     if not args.canonical:
         args.canonical = tool.manifest.get("canonical", False)
@@ -419,22 +422,22 @@ if __name__ == "__main__":
             "web::extra_args", template_extra_args
         )
 
-    if args.type not in acceptable_types.keys():
+    if args.wstype not in acceptable_wstypes.keys():
         print("type must be one of:")
-        print(format_types_block(acceptable_types, ""))
+        print(format_wstypes_block(acceptable_wstypes, ""))
         sys.exit(1)
 
     if backend == "gridengine":
         job = GridEngineBackend(
             tool,
-            args.type,
+            args.wstype,
             canonical=args.canonical,
             extra_args=args.extra_args,
         )
     else:
         job = KubernetesBackend(
             tool,
-            args.type,
+            args.wstype,
             canonical=args.canonical,
             mem=args.memory,
             cpu=args.cpu,
@@ -444,8 +447,8 @@ if __name__ == "__main__":
 
     tool.manifest["backend"] = backend
 
-    if job.is_deprecated(args.type):
-        print("DEPRECATED: '{}' type is deprecated.".format(args.type))
+    if job.is_deprecated(args.wstype):
+        print("DEPRECATED: '{}' type is deprecated.".format(args.wstype))
         print(
             "  See https://wikitech.wikimedia.org/wiki/Help:Toolforge/{}".format(
                 "Kubernetes" if backend == "kubernetes" else "Web"
@@ -480,7 +483,7 @@ if __name__ == "__main__":
         if job.get_state() != Backend.STATE_STOPPED:
             print(
                 "Your webservice of type {} is running on backend {}".format(
-                    job.type, backend
+                    job.wstype, backend
                 ),
                 file=sys.stdout,
             )

scripts/webservice-runner
@@ -8,6 +8,7 @@ from toolforge.webservice.services import webservice_classes
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
     "--type",
+    dest="wstype",
     help="Type of webservice to start",
     choices=webservice_classes.keys(),
 )
@@ -37,7 +38,7 @@ if args.port != 0:
 else:
     port = proxy.get_open_port()
 
-webservice = webservice_classes[args.type](tool, args.extra_args)
+webservice = webservice_classes[args.wstype](tool, args.extra_args)
 
 if args.register_proxy:
     proxy.register(port, args.canonical)

toolforge/webservice/backends/gridenginebackend.py
@@ -45,8 +45,8 @@ class GridEngineBackend(Backend):
         cfg = GridEngineBackend.CONFIG[self.wstype]
         self.webservice = cfg["cls"](tool, extra_args)
         self.queue = cfg.get("queue", "webgrid-generic")
-        self.name = "{type}-{toolname}".format(
-            type=self.wstype, toolname=tool.name
+        self.name = "{wstype}-{toolname}".format(
+            wstype=self.wstype, toolname=tool.name
         )
         try:
             memlimit = "/data/project/.system/config/{}.web-memlimit".format(

toolforge/webservice/backends/kubernetesbackend.py
@@ -170,7 +170,7 @@ class KubernetesBackend(Backend):
             tool, wstype, canonical=canonical, extra_args=extra_args
         )
         self.project = PROJECT
-        self.webservice = KubernetesBackend.CONFIG[type]["cls"](
+        self.webservice = KubernetesBackend.CONFIG[self.wstype]["cls"](
             tool, extra_args
         )
 
@@ -180,7 +180,7 @@ class KubernetesBackend(Backend):
 
         self.container_image = "{registry}/{image}:latest".format(
             registry="docker-registry.tools.wmflabs.org",
-            image=KubernetesBackend.CONFIG[type]["image"],
+            image=KubernetesBackend.CONFIG[self.wstype]["image"],
         )
         # Defaults are used for request so this just affects the burst, up to
         # certain limits (set as max in the limitrange) as well as