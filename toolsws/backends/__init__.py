616bc45ae017d6348bcc4854ce634848078bdd15
Raymond Ndibe <rolisaemeka-ctr@wikimedia.org>
2023-06-19 14:58:25 +0100

    Moved to gitlab
    https://gitlab.wikimedia.org/repos/cloud/toolforge/tools-webservice
    
    Bug: T331335
    Signed-off-by: Raymond Ndibe <rolisaemeka-ctr@wikimedia.org>
    Change-Id: Ibe6590a89fd55550b851cc96451f504ba7605a3a

--- toolsws/backends/__init__.py
+++
@@ -1,6 +0,0 @@
-from .backend import Backend
-from .gridengine import GridEngineBackend
-from .kubernetes import KubernetesBackend
-
-
-__all__ = ["Backend", "GridEngineBackend", "KubernetesBackend"]
